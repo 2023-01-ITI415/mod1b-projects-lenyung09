3e4f013a33882bfe7a52b41c47db3bd0928f5d98
Leo Cheng <71459641+lenyung09@users.noreply.github.com>
2023-02-09 20:04:08 -0500

    FollowCam Script Implementation

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -71,6 +71,7 @@ public class Slingshot : MonoBehaviour
             projRB.isKinematic = false;
             projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
             projRB.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;
         }
     }
